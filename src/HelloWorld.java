04a97698a1c7d9dd9b16840641f6ff92b002277f
miki1989 <mikolajheller@gmail.com>
2018-07-02 10:58:33 +0200

    Stworzony plik HelloWorld.java

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("a");
+        System.out.println("b");
+        System.out.println("c");
+        System.out.println("d");
+        System.out.println("e");
+        System.out.println("f");
+
+        System.out.println("Cześć. Działa");
+
+    }
+}
